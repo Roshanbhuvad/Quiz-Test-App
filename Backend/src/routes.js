3cfe5ae49d9d651cc9967ecfaedc2ed0e6fb37e0
Roshan <roshanbhuvad15@gmail.com>
2020-09-21 20:58:39 +0530

    first POST route created for new question

--- Backend/src/routes.js
+++ Backend/src/routes.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const router = express.Router()
-
+const Question = require("../models/Question")
 // Get All quiz questions
 router.get("/questions", (req, res) => {
 
@@ -12,7 +12,26 @@ router.get("/question/:id", (req, res) => {
 })
 
 // Create one quiz question 
-router.post("/questions", (req, res) => {
+router.post("/questions", async (req, res) => {
+    try {
+        const {
+            description
+        } = req.body
+        const {
+            alternatives
+        } = req.body
+
+        const question = await Question.create({
+            description,
+            alternatives
+        })
+
+        return res.status(201).json(question)
+    } catch (error) {
+        return res.status(500).json({
+            "error": error
+        })
+    }
 
 })
 
