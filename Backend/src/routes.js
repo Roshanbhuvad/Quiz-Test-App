0ac7ef1e86c5553e9c8d11e21095c2249ff6c490
Roshan <roshanbhuvad15@gmail.com>
2020-09-21 19:52:24 +0530

    working on API routes

---
+++ Backend/src/routes.js
@@ -0,0 +1,29 @@
+const express = require("express")
+const router = express.Router()
+
+// Get All quiz questions
+router.get("/questions", (req, res) => {
+
+})
+
+// Get one quiz question
+router.get("/question/:id", (req, res) => {
+
+})
+
+// Create one quiz question 
+router.post("/questions", (req, res) => {
+
+})
+
+// Update one quiz question
+router.put("/questions/:id", (req, res) => {
+
+})
+
+// Delete one quiz question
+router.delete("/questions/:id", (req, res) => {
+
+})
+
+module.exports = router
--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,7 +1,14 @@
 const express = require("express")
 const app = express()
 const mongoose = require("mongoose")
+const routes = require("./routes")
+const cors = require("cors")
 
+require("dotenv").config()
+
+app.use(cors()) // telling express to use CORS
+app.use(express.json()) // server to use json as well
+app.use(routes) // server to use the routes in routes.js
 mongoose.connect(process.env.DATABASE_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true
